c5ebf607249616768c531a2fec4beef02c48256a
Queen Mueni <qmueni@hq.ekenya.co.ke>
2019-09-05 09:45:24 +0300

    my first navigation graph

---
+++ app/src/main/java/ke/ac/navgraph/MainActivity.java
@@ -0,0 +1,14 @@
+package ke.ac.navgraph;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
